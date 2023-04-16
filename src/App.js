2685c6111f363a40813922003493e628327d78c6
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2023-04-16 20:32:10 +0300

    clean

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
   );
