f17f69ea81465494289fcba5f1f02acb2a8723c6
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2023-04-16 22:13:19 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
-        <Route path="/*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   );
