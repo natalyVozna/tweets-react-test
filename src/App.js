e5b5ab0517015a5abdc2a6ae4e1838fadc3491ee
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2023-04-17 18:16:29 +0300

    fix router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,19 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import Tweets from './pages/Tweets/Tweets';
 import GlobalStyle from './globalStyles';
-import NotFound from './components/NotFound/NotFound';
-
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Home />,
-      errorElement: <NotFound />,
-    },
-    {
-      path: '/tweets',
-      element: <Tweets />,
-      errorElement: <NotFound />,
-    },
-  ],
-  {
-    basename: '/tweets-react-test',
-  }
-);
-
-// function App() {
-//   return (
-//     <>
-//       <GlobalStyle />
-//       <Routes>
-//         <Route index element={<Home />} />
-//         <Route path="/tweets" element={<Tweets />} />
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-//     </>
-//   );
-// }
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <RouterProvider router={router} />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/tweets" element={<Tweets />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 }
