dc446543ca4542a5f6771270bcfac70efe4befaf
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2023-04-17 16:35:31 +0300

    fix router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import { Route, Routes } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import Tweets from './pages/Tweets/Tweets';
 import GlobalStyle from './globalStyles';
 import NotFound from './components/NotFound/NotFound';
 
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Home />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: '/tweets',
+      element: <Tweets />,
+    },
+  ],
+  {
+    basename: '/tweets-react-test',
+  }
+);
+
+// function App() {
+//   return (
+//     <>
+//       <GlobalStyle />
+//       <Routes>
+//         <Route index element={<Home />} />
+//         <Route path="/tweets" element={<Tweets />} />
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </>
+//   );
+// }
+
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/tweets" element={<Tweets />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   );
 }
