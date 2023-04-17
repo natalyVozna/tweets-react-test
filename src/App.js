82e318e1358c3976d783561c1b9992818010e8ee
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2023-04-17 09:23:57 +0300

    add notfound

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home/Home";
-import Tweets from "./pages/Tweets/Tweets";
-import GlobalStyle from "./globalStyles";
-import NotFound from "./components/NotFound/NotFound";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home/Home';
+import Tweets from './pages/Tweets/Tweets';
+import GlobalStyle from './globalStyles';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
-  const location = useLocation();
-  console.log(location, "location");
   return (
     <>
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/notfound" element={<NotFound />} />
       </Routes>
     </>
   );
